0900aa563976aaabd286ab11c0a648385091447f
UrbanProfessional <spytron2005@gmail.com>
2025-07-09 14:39:22 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>New York Cloud Storage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body id="landingBody">
+    <h1>By New Yorkers. For New Yorkers</h1>
+    <p>No ads. No tracking. Just secure cloud storage.</p>
+</body>
+</html>
